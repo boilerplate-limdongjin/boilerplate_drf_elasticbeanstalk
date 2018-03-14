207b91bf79b906cdb132505d30b8ca6be076a87d
Ubuntu <e20161631@sogang.ac.kr>
2018-03-14 07:53:17 +0000

    #2. update setting

--- iotd/iotd/settings.py
+++ iotd/iotd/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'iotd.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-if 'RDS_HOSTNAME' in os.environ:
+try:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
@@ -82,7 +82,7 @@ if 'RDS_HOSTNAME' in os.environ:
             'PORT': os.environ['RDS_PORT'],
         }
     }
-else:
+except:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
